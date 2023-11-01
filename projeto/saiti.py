a66e7b27d92b2b4ee8b357b69b24b866494a5452
kaue121212 <149586053+kaue121212@users.noreply.github.com>
2023-11-01 08:54:55 -0300

    Add files via upload

---
+++ projeto/saiti.py
@@ -0,0 +1,49 @@
+from flask import Flask, redirect, render_template, url_for
+import sqlite3
+ 
+app = Flask(__name__)
+ 
+AGENDA = 'agenda.db'
+
+def conectar_bd():
+    return sqlite3.connect(AGENDA)
+
+with conectar_bd() as con:
+    con.execute('''CREATE TABLE IF NOT EXISTS pessoa (id INTEGER PRIMARY KEY AUTOINCREMENT, nome TEXT,email TEXT)''')
+
+@app.route('/')
+def listar_registros():
+    with conectar_bd() as con:
+        registros = con.execute('SELECT * FROM pessoa').fetchall()
+    return render_template('listagem.html', registros=registros)
+
+@app.route('/adicionar', methods=['POST'])
+def adicionar_registro():
+    nome =  request.form['nome']
+    email = request.form['email']
+    with conectar_bd() as con:
+        con.execute('INSERT INTO registros (nome, email) VALUES (?,?)', (nome, email))
+    return redirect(url_for('listar_registros'))
+
+@app.route('/editar/<int:id>')
+def editar_registro(id):
+    with conectar_bd() as con:
+        registro = con.execute('SELECT * FROM registros WHERE id =?', (id,)).fetchone()
+    return render_template('editar.html', registro=registro)
+
+@app.route('/atualizar/<int:id>', methods=['POST'])
+def atualizar_registro(id):
+    nome = request.form['nome']
+    email = request.form['email']
+    with conectar_bd() as con:
+        con.execute('UPDATE registros SET nome=?, email=? WHEREid=?', (nome, email, id))
+    return redirect(url_for('listar_registros'))
+
+@app.route('/excluir/<int:id>')
+def excluir_registro(id):
+    with conectar_bd() as con:
+        con.execute('DELETE FROM registros WHERE id = ?', (id,))
+    return redirect(url_for('listar_registros'))
+
+if __name__ == '__main__':   
+    app.run(debug=True)
